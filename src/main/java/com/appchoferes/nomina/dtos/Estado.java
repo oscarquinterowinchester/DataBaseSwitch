9cf230e887bd120b24ebe8a1f810b1ce62ca61fc
oscarquinterowinchester <alejandrox2405@gmail.com>
2024-07-19 16:10:16 -0700

    Estado

---
+++ src/main/java/com/appchoferes/nomina/dtos/Estado.java
@@ -0,0 +1,12 @@
+package com.appchoferes.nomina.dtos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "estados_tbl")
+public class Estado {
+
+    
+
+}
